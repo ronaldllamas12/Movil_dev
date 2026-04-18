07f8682c1ba3963a52d7719a618e5d2f32b80247
Ronald <ronaldllamas17@gmail.com>
2026-04-18 08:33:35 -0500

    Actualizar main.py

--- backend/main.py
+++ backend/main.py
@@ -42,6 +42,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:5173",
         "http://localhost:5173",
+        "https://movil-dev.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
