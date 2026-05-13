667e9fa8eaf17072df32948d60794ee467da7fc8
SantiagoT <122948175+SantiagoTorresH@users.noreply.github.com>
2026-05-13 18:17:45 -0500

    fix little issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
               "src/**/*.spec.*",
               "src/test/**",
             ],
-            extension: [".js", ".jsx"],
+            extension: [".js", ".jsx", ".ts", ".tsx"],
             requireEnv: false,
           }),
         ]
